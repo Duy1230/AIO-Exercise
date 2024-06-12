5c24af25cd3168ed3369e6a80ca4aaa168d3335e
Duy1230 <21522000@gm.uit.edu.vn>
2024-06-12 14:06:41 +0700

    Fixed-Variable-Name-SonarCloud

--- module-1/week-1/exercise-2.py
+++ module-1/week-1/exercise-2.py
@@ -1,13 +1,16 @@
 
-def is_number (n) :
-    try :
-        float (n) 
+import math
 
-    except ValueError :
+
+def is_number(n):
+    try:
+        float(n)
+
+    except ValueError:
         return False
     return True
 
-import math
+
 def cal_activation():
     activations = ['sigmoid', 'relu', 'elu']
     number = input("Input x= ")
@@ -15,19 +18,20 @@ def cal_activation():
         print("x must be a number")
         return
     number = float(number)
-    function = input("Input activation function (sigmoid, relu, elu): ")
-    if function not in activations:
-        print(f"{function} is not a supported activation function")
-    
-    if function == 'sigmoid':
+    function_name = input("Input activation function (sigmoid, relu, elu): ")
+    if function_name not in activations:
+        print(f"{function_name} is not a supported activation function")
+
+    if function_name == 'sigmoid':
         print(f"sigmoid f({number}) = {1/(1+math.exp(-1*number))}")
-    if  function == 'relu':
-        print(f"ReLu f({number}) = {max(0,number)}")
-    if function == 'elu':
+    if function_name == 'relu':
+        print(f"ReLu f({number}) = {max(0, number)}")
+    if function_name == 'elu':
         if number <= 0:
             print(f"Elu f({number}) = {0.01*(math.exp(number)-1)}")
         else:
             print(f"Elu f({number}) = {number}")
+
 
 if __name__ == "__main__":
     cal_activation()
